4652c0f9813a8bce917514bfe46bcc3b999ac2eb
volodymyrLukashchuk <92587226+volodymyrLukashchuk@users.noreply.github.com>
2022-04-26 17:42:06 -0500

    fix categories

---
+++ src/components/Shop/Products.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Card from "../Card/Card";
+
+import { useSelector, useDispatch } from "react-redux";
+import { getLastProducts } from "../../features/redux/bazar/bazarThunkActions";
+import { getProductsSelector } from "../../features/redux/bazar/bazarSelector";
+
+const Products = () => {
+  const dispatch = useDispatch();
+  const products = useSelector(getProductsSelector);
+
+  const getLastProductsHandler = () => {
+    dispatch(getLastProducts());
+  };
+
+  return (
+    <div>
+      <div>
+        <Card products={products} />
+      </div>
+      <div className="load-button">
+        {products.length !== 0 && products.length < 30 ? (
+          <button onClick={getLastProductsHandler}>Load More</button>
+        ) : null}
+      </div>
+    </div>
+  );
+};
+
+export default Products;
